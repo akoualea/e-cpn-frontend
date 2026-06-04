2e79055e5eb80b8cf39d0ae98a808e2b02825bad
akoualea <akoualeasowanou@gmail.com>
2026-06-04 13:06:53 +0100

    Update Dashboard.jsx

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -12,7 +12,7 @@ import logo from '../../assets/logo.png';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: https://e-cpn-backend-production.up.railway.app/,
+    baseURL: 'http://127.0.0.1:8000/api',
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
